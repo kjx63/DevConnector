a59d90b1c8d70db1538c8dac2a5d20d5a8f8c08e
kjx63 <ueyama.kenji.63@gmail.com>
2020-08-16 09:45:43 +0900

    Create Profile Component and Dashboard

--- devconnector/client/src/components/layout/Navbar.js
+++ devconnector/client/src/components/layout/Navbar.js
@@ -3,10 +3,15 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
-
-const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+const Navbar = ({ auth: { isAuthenticated, user, loading }, logout }) => {
   const authLinks = (
     <ul>
+      <li>
+        <Link to='/dashboard'>
+          <i className='fas fa-user' />{' '}
+          <span className='hide-sm'>{user && user.name}</span>
+        </Link>
+      </li>
       <li>
         <a onClick={logout} href='#!'>
           <i className='fas fa-sign-out-alt' />{' '}
